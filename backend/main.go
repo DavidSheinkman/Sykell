57ad45565a4c84a3bac5c9f57de3307a4dae8e39
DavidSheinkman <Poilkjmnb!davidsheinkman7>
2025-07-08 00:01:32 +0200

    frontend & backend - initial project scaffolding

---
+++ backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	dsn := "user:pass@tcp(db:3306)/crawler?parseTime=true"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Cannot connect to DB:", err)
+	}
+
+	fmt.Println("Connected to MySQL!")
+}
